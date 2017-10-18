641f42c71096ad5df75abba31b0c24d102a1b784
wdc596933938 <wdc596933938@163.com>
2017-10-18 10:37:08 +0800

    add new code for test

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+ * @Author: 汪德成
+ * @Date: 2017-10-18 08:48:25 
+ * @Last Modified by: 汪德成
+ * @Last Modified time: 2017-10-18 08:48:49
+ */
+
+#include "stdio.h"
+
+int main()
+{
+    printf("hello world!\n");
+    return 0;
+}
